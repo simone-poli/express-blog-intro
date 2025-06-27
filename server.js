8411459d7d5819f8d04e10cf5768274886a33ec8
simone-poli <polismn97@gmail.com>
2025-06-27 16:02:50 +0200

    feat: serve static files and add API endpoint for blog posts

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+app.use(express.static("public"))
+
+
+
 
 const posts = [
     {
@@ -9,25 +13,25 @@ const posts = [
         contenuto: "Torta rotonda",
         immagine: "public/img/ciambellone.jpeg",
         tags: ["mangiare", "dolce", "ricetta"]
-    }
+    },
     {
         titolo: "Cracker alla barbabietola",
         contenuto: "Cracker colorati",
         immagine: "public/img/cracker_barbabietola.jpeg",
         tags:["mangiare", "dieta", "dolcicolorati"]
-    }
+    },
     {
         titolo: "Pane fritto dolce",
         contenuto: "Pane dolce",
         immagine: "public/img/pane_fritto_dolce.jpeg",
         tags: ["pane", "cibo","dolce"]
-    }
+    },
     {
         titolo: "Pasta alla barbabietola",
         contenuto: "Pasta colorata",
         immagine: "public/img/pasta_barbabietola.jpeg",
         tags: ["mangiare", "primo", "barbabietola"]
-    }
+    },
     {
         titolo: "Torta paesana",
         contenuto: "Torta tipica",
@@ -46,4 +50,8 @@ app.get("/",(req, res) =>{
 
 app.listen (port, ()=>{
     console.log(`The server is start and listening in http://localhost:${port}`)
+})
+
+app.get("/api/posts", (req, res) => {
+    res.json(posts)
 })
