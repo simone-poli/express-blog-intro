1576a5fbb079f128bf646c9a1ec4818b2952760e
simone-poli <polismn97@gmail.com>
2025-06-27 15:59:36 +0200

    feat: add initial blog posts and images to server

--- server.js
+++ server.js
@@ -3,6 +3,42 @@ const app = express()
 const port = 3000
 
 
+const posts = [
+    {
+        titolo: "Ciambellone",
+        contenuto: "Torta rotonda",
+        immagine: "public/img/ciambellone.jpeg",
+        tags: ["mangiare", "dolce", "ricetta"]
+    }
+    {
+        titolo: "Cracker alla barbabietola",
+        contenuto: "Cracker colorati",
+        immagine: "public/img/cracker_barbabietola.jpeg",
+        tags:["mangiare", "dieta", "dolcicolorati"]
+    }
+    {
+        titolo: "Pane fritto dolce",
+        contenuto: "Pane dolce",
+        immagine: "public/img/pane_fritto_dolce.jpeg",
+        tags: ["pane", "cibo","dolce"]
+    }
+    {
+        titolo: "Pasta alla barbabietola",
+        contenuto: "Pasta colorata",
+        immagine: "public/img/pasta_barbabietola.jpeg",
+        tags: ["mangiare", "primo", "barbabietola"]
+    }
+    {
+        titolo: "Torta paesana",
+        contenuto: "Torta tipica",
+        immagine: "public/img/torta_paesana.jpeg",
+        tags: ["mangiare", "dolce", "ricetta"]
+    }
+]
+
+
+
+
 
 app.get("/",(req, res) =>{
     res.send("<h1>Server del mio blog<h1>")
